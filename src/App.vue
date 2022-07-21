7401906387e08c0ab2976c1c2f9f3e61016c9277
Paulo Sergio Klaus <contato@pauloklaus.com.br>
2022-07-21 17:10:39 -0300

    feat: add vuejs framework

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Weather Conditions</h1>
+</template>
+
+<script>
+import { defineComponent } from "@vue/composition-api"
+
+export default defineComponent({
+  setup() {
+    cities = []
+    citiesServices = CitiesFactory.factory()
+
+    Promise.all()
+  },
+})
+</script>
