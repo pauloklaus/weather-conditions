de21ba9583039c74af69db04d20f0c8809f9f101
Paulo Sergio Klaus <contato@pauloklaus.com.br>
2022-07-22 03:50:22 -0300

    test: validators

---
+++ tests/unit/helpers/IsEmpty.spec.js
@@ -0,0 +1,23 @@
+import isEmpty from "@/helpers/IsEmpty";
+
+describe("IsEmpty", () => {
+
+  it("should return true with an empty string", () => {
+    expect(isEmpty("")).toBeTruthy();
+    expect(isEmpty(" ")).toBeTruthy();
+    expect(isEmpty()).toBeTruthy();
+    expect(isEmpty(undefined)).toBeTruthy();
+    expect(isEmpty(null)).toBeTruthy();
+  });
+
+  it("should return false with an valid string", () => {
+    expect(isEmpty("t")).toBeFalsy();
+    expect(isEmpty("test")).toBeFalsy();
+    expect(isEmpty(" test ")).toBeFalsy();
+    expect(isEmpty("0")).toBeFalsy();
+    expect(isEmpty(0)).toBeFalsy();
+    expect(isEmpty("null")).toBeFalsy();
+    expect(isEmpty("undefined")).toBeFalsy();
+  });
+
+});
