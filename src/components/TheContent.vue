2e40152a136b7ec809052493f134f4678aba48f6
Paulo Sergio Klaus <contato@pauloklaus.com.br>
2022-07-24 00:19:03 -0300

    feat: auto rotate card details

--- src/components/TheContent.vue
+++ src/components/TheContent.vue
@@ -1,22 +1,66 @@
 <template>
   <main class="main">
     <div class="main__container">
-      <CityCard v-for="city in cities" :key="city" :city="city" />
+      <CityCard
+        v-for="(city, index) in cities"
+        :key="city"
+        :city="city"
+        :show-details="isCurrentShowDetails(index)"
+        @mouseover="showDetails(index)"
+      />
     </div>
   </main>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, onBeforeUnmount, ref } from "vue";
 import CityCard from "./CityCard/CityCard.vue";
 
 export default defineComponent({
   components: { CityCard },
   setup() {
     const cities = ["nuuk,gl", "urubici,br", "nairobi,ke"];
+    const detailsIndex = ref(0);
+
+    function isCurrentShowDetails(index) {
+      return detailsIndex.value === index;
+    }
+
+    function showDetails(index) {
+      return detailsIndex.value = index;
+    }
+
+    function clearDetailsTimeout() {
+      if (detailsTimeout) {
+        clearTimeout(detailsTimeout);
+      }
+    }
+
+    function showNextDetails() {
+      clearDetailsTimeout();
+
+      detailsTimeout = setTimeout(() => {        
+        detailsIndex.value = 
+          detailsIndex.value < cities.length - 1
+            ? detailsIndex.value + 1
+            : 0;
+
+        showNextDetails();
+      }, 2_000);
+    }
+
+    let detailsTimeout = null;
+
+    showNextDetails();
+
+    onBeforeUnmount(() => {
+      clearDetailsTimeout();
+    });
 
     return {
       cities,
+      isCurrentShowDetails,
+      showDetails,
     };
   },
 });
