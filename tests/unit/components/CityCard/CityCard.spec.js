4dd50f496ece9c809a540629f7ab49c6e3ed4669
Paulo Sergio Klaus <contato@pauloklaus.com.br>
2022-07-24 01:58:39 -0300

    test: to cover new features

--- tests/unit/components/CityCard/CityCard.spec.js
+++ tests/unit/components/CityCard/CityCard.spec.js
@@ -1,10 +1,13 @@
 import { shallowMount } from "@vue/test-utils";
 import CityCard from "@/components/CityCard/CityCard.vue";
 import CityCardHeader from "@/components/CityCard/CityCardHeader.vue";
+import CityCardError from "@/components/CityCard/CityCardError.vue";
 import CityCardContent from "@/components/CityCard/CityCardContent.vue";
 import CityCardFooter from "@/components/CityCard/CityCardFooter.vue";
+import CityCardLoader from "@/components/CityCard/CityCardLoader.vue";
 
 describe("CityCard.vue", () => {
+
   const wrapper = shallowMount(CityCard, { props: { city: "sao paulo,br" }});
   const cardElement = wrapper.find("div");
 
@@ -16,11 +19,24 @@ describe("CityCard.vue", () => {
     expect(cardElement.findComponent(CityCardHeader).exists()).toBeTruthy();
   });
 
-  it("should find CityCardContent element", () => {
-    expect(cardElement.findComponent(CityCardContent).exists()).toBeTruthy();
+  it("should find CityCardError element", () => {
+    expect(cardElement.findComponent(CityCardError).exists()).toBeTruthy();
   });
 
-  it("should find CityCardFooter element", () => {
+  it("should find CityCardLoading element", async () => {
+    wrapper.vm.hasError = false;
+    wrapper.vm.isLoading = true;
+    await wrapper.vm.$nextTick();
+
+    expect(cardElement.findComponent(CityCardLoader).exists()).toBeTruthy();
+  });
+
+  it("should find CityCardContent and CityCardFooter element", async () => {
+    wrapper.vm.hasError = false;
+    wrapper.vm.isLoading = false;
+    await wrapper.vm.$nextTick();
+
+    expect(cardElement.findComponent(CityCardContent).exists()).toBeTruthy();
     expect(cardElement.findComponent(CityCardFooter).exists()).toBeTruthy();
   });
 
