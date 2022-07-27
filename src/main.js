28d41e0356b38ec9441cb417a6c61d0b6dac9d71
Paulo Sergio Klaus <contato@pauloklaus.com.br>
2022-07-26 21:21:32 -0300

    feat: pinia store lib

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import { createApp } from "vue";
+import { createPinia } from "pinia";
 import App from "./App.vue";
 
 import "./assets/css/root-vars.css";
 import "./assets/css/base.css";
 
-createApp(App).mount("#app");
+const app = createApp(App);
+
+app.use(createPinia());
+app.mount("#app");
